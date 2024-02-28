319e562000eac7b4d4ab812ff6fd9396db2a5584
anagarwa <anagarwa@adobe.com>
2024-02-28 13:13:13 +0530

    fix: working carousal

---
+++ blocks/carousel1/carousel.js
@@ -0,0 +1,195 @@
+
+
+const debounceDelay = 30;
+
+function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 400px)', width: '2000' }, { width: '750' }]) {
+  const url = new URL(src, getHref());
+  const picture = document.createElement('picture');
+  const {pathname} = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    source.setAttribute('srcset', `${pathname}?width=${br.width}&format=webply&optimize=medium`);
+    picture.appendChild(source);
+  });
+}
+function adjustWidthAndControls(block, carousel, ...controls) {
+  function toggle() {
+    const documentWidth = document.documentElement.clientWidth;
+    const isDesktop = documentWidth > 767 && !block.matches('.grid-on-desktop');
+    const gap = parseInt(window.getComputedStyle(carousel).gap, 10);
+    const itemWidth = parseInt(window.getComputedStyle(carousel.firstElementChild).width, 10);
+    const itemsWidth = itemWidth * carousel.children.length + gap * (carousel.children.length - 1);
+    let containerWidth = parseInt(window.getComputedStyle(carousel.parentElement).width, 10);
+    if (isDesktop) {
+      // on desktop the container width is 2x50px smaller due to the controls
+      containerWidth -= 100;
+    }
+    const showControls = itemsWidth > containerWidth;
+    controls.forEach((ul) => (showControls ? ul.classList.remove('hidden') : ul.classList.add('hidden')));
+    if (showControls) carousel.classList.remove('centered'); else carousel.classList.add('centered');
+    if (isDesktop) {
+      // set the width only on desktop
+      const maxItems = Math.floor((containerWidth - 100) / (itemsWidth / carousel.children.length));
+      const width = maxItems * itemWidth + gap * (maxItems - 1);
+      carousel.style.width = `${width}px`;
+    } else {
+      // remove on mobile viewports
+      delete carousel.style.width;
+    }
+  }
+
+  let resizeTimeout;
+  window.addEventListener('resize', () => {
+    if (resizeTimeout) clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(toggle, debounceDelay);
+  });
+
+  // wait for the section to be loaded before we initially resize the carousel
+  const section = block.closest('.section');
+  new MutationObserver((_, observer) => {
+    if (section.dataset.sectionStatus === 'loaded') {
+      observer.disconnect();
+      setTimeout(toggle);
+    }
+  }).observe(section, { attributes: true });
+}
+
+function createDesktopControls(ul) {
+  function scroll(direction) {
+    const first = ul.firstElementChild;
+    const second = first.nextElementSibling;
+    const scrollOffset = second.getBoundingClientRect().x - first.getBoundingClientRect().x;
+    const left = direction === 'left' ? -1 * scrollOffset : scrollOffset;
+    ul.scrollBy({ top: 0, left, behavior: 'smooth' });
+  }
+
+  const desktopControls = document.createElement('ul');
+  desktopControls.className = 'desktop-controls hidden';
+  desktopControls.innerHTML = '<li><button type="button">Left</button></li><li><button type="button">Right</button></li>';
+  ul.insertAdjacentElement('beforebegin', desktopControls);
+  const [prevButton, nextButton] = desktopControls.querySelectorAll(':scope button');
+  prevButton.addEventListener('click', () => scroll('left'));
+  nextButton.addEventListener('click', () => scroll('right'));
+  return desktopControls;
+}
+
+function createMobileControls(ul) {
+  // create carousel controls for mobile
+  const mobileControls = document.createElement('ul');
+  mobileControls.className = 'mobile-controls';
+  [...ul.children].forEach((item, j) => {
+    const control = document.createElement('li');
+    if (!j) control.className = 'active';
+    control.innerHTML = `<button type="button">${j + 1}</button>`;
+    control.firstElementChild.addEventListener('click', () => {
+      mobileControls.querySelector('li.active').classList.remove('active');
+      control.classList.add('active');
+
+      const left = item.offsetLeft + item.offsetWidth / 2
+        - (item.parentNode.offsetLeft + item.parentNode.offsetWidth / 2);
+      ul.scrollTo({ top: 0, left, behavior: 'smooth' });
+    });
+
+    mobileControls.append(control);
+  });
+  ul.insertAdjacentElement('beforebegin', mobileControls);
+
+  let scrollTimeout;
+  ul.addEventListener('scroll', () => {
+    if (scrollTimeout) clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      const first = ul.firstElementChild;
+      const second = first.nextElementSibling;
+      const scrollOffset = second.getBoundingClientRect().x - first.getBoundingClientRect().x;
+      let index = 0;
+      // how many items have scrolled out?
+      while (ul.scrollLeft - scrollOffset * (index + 1) > 0) index += 1;
+      mobileControls.querySelector('li.active').classList.remove('active');
+      mobileControls.children[index].classList.add('active');
+    }, debounceDelay);
+  });
+  return mobileControls;
+}
+
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+  ul.classList.add('items');
+
+  // We support two formats:
+  // 1. Each slide in a cell in either columns column and/or rows.
+  // 2. all values in the first cell as a list.
+  [...block.querySelectorAll(':scope > div > div')].forEach((cell) => {
+    // collect any <li>
+    cell.querySelectorAll('li').forEach((li) => {
+      if (li.childElementCount) {
+        ul.append(li);
+      }
+    });
+    // remove any remaining <ul>
+    cell.querySelectorAll('ul').forEach((el) => el.remove());
+
+    // If cell still contains anything, add content to list
+    if (cell.childElementCount) {
+      const li = document.createElement('li');
+      li.append(...cell.childNodes);
+      // remove link decoration
+      li.querySelectorAll('.button-container,.button').forEach((el) => {
+        el.classList.remove('button-container', 'button', 'primary');
+      });
+      ul.append(li);
+    }
+    cell.remove();
+  });
+
+  // now that all cells are removed, only rows are remining.
+  const firstCell = document.createElement('div');
+  block.firstElementChild.append(firstCell);
+  firstCell.append(ul);
+
+  [...ul.children].forEach((li) => {
+    // Add wrapper around the content
+    const container = document.createElement('div');
+    container.className = 'carousel-content-wrapper';
+    container.innerHTML = li.innerHTML;
+    li.innerHTML = '';
+    li.append(container);
+
+    // add link to the image and move it outside of the wrapper
+    const a = li.querySelector('a');
+    let picture = li.querySelector('picture');
+    if (picture) {
+      const img = picture.lastElementChild;
+      const newPicture = createOptimizedPicture(img.src, img.alt, false, [{ width: '370' }]);
+      picture.replaceWith(newPicture);
+      picture = newPicture;
+    }
+    if (a && picture) {
+      const clone = a.cloneNode(false);
+      picture.replaceWith(clone);
+      clone.append(picture);
+      li.prepend(clone);
+    }
+
+    const textItems = container.innerHTML
+      .split('<br>').filter((text) => text.trim() !== '');
+
+    container.innerHTML = `
+      <p>${textItems.join('</p><p>')}</p>
+    `;
+
+    const carouselLink = container.querySelector('a');
+    if (carouselLink) {
+      carouselLink.classList.add('button', 'carousel-link');
+    }
+  });
+
+  const desktopControls = createDesktopControls(ul);
+  const mobileControls = createMobileControls(ul);
+
+  adjustWidthAndControls(block, ul, mobileControls, desktopControls);
+}
--- blocks/tradingideas/tradingideas.js
+++ blocks/tradingideas/tradingideas.js
@@ -1,12 +1,16 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-    /* change to ul, li */
-//    block.content=' i am gere';
     block.textContent = '';
+
+    const explorerSection = document.createElement('div');
+    explorerSection.classList.add('explore-section');
+    block.appendChild(explorerSection);
+
+    explorerSection.appendChild(generateHtmlForIdeasStrike());
     const explorerContainer = document.createElement('div');
     explorerContainer.className = 'explore-container';
-    block.appendChild(explorerContainer);
+    explorerSection.appendChild(explorerContainer);
 
     const explorerHeader     = document.createElement('div');
     explorerHeader.className = 'explore-header';
@@ -29,35 +33,100 @@ export default function decorate(block) {
     researchSlider.classList.add('slick-initialized');
     researchSlider.classList.add('slick-slider');
     researchSlider.classList.add('slick-dotted');
-    explorerBody.appendChild(researchSlider);
+    researchSlider.classList.add('carousel');
+
     const slickList     = document.createElement('div');
     slickList.classList.add('slick-list');
     slickList.classList.add('draggable');
     researchSlider.appendChild(slickList);
-
     const slickTrack     = document.createElement('div');
     slickTrack.classList.add('slick-track');
     slickList.appendChild(slickTrack);
+    explorerBody.appendChild(researchSlider);
 
+    fetchAndProcessCompaniesData().then(companies => {
+        if (companies) {
+            const htmlElementsArray = generateHtmlForCompanyCard(companies);
+            htmlElementsArray.forEach((htmlString, index) => {
+                const div = document.createElement('div');
+                div.innerHTML = htmlString;
+                const element = div.firstElementChild;
+                element.style.display = 'none'; // Default all to 'none' initially
+                slickTrack.appendChild(element);
+            });
 
+            // Initialize the carousel view with the first 3 cards
+            const cards = Array.from(slickTrack.children);
+            for (let i = 0; i < 4 && i < cards.length; i++) {
+                cards[i].style.display = 'block';
+            }
 
+            // Dot Navigation
+            const dotsContainer = document.createElement('div');
+            dotsContainer.className = 'dots-container';
+            researchSlider.appendChild(dotsContainer);
 
+            // Assuming you have 8 cards and can show 3 at a time, calculate the number of dots needed
+            const numberOfDots = Math.ceil((cards.length - 3)); // For 8 cards, this results in 6 dots
+            const dots = []; // To keep track of all dot elements
 
+            for (let i = 0; i < numberOfDots; i++) {
+                const dot = document.createElement('button');
+                dot.className = 'dot';
+                dot.dataset.index = i;
+                dotsContainer.appendChild(dot);
+                dots.push(dot);
 
-    const htmlElementsArray = generateHtmlFromArray(companies);
-    htmlElementsArray.forEach(htmlString => {
-        const div = document.createElement('div');
-        div.innerHTML = htmlString;
-        const element = div.firstElementChild;
-        slickTrack.appendChild(element);
+                dot.addEventListener('click', function() {
+                    updateCarouselView(parseInt(this.dataset.index));
+                    updateActiveDot(parseInt(this.dataset.index));
+                });
+            }
 
+            const updateCarouselView = (dotIndex) => {
+                // Hide all cards
+                cards.forEach(card => card.style.display = 'none');
+
+                // Calculate the range of cards to show based on dot clicked
+                const startIndex = dotIndex;
+                const endIndex = startIndex + 4 ;
+
+                // Show the cards in the range
+                for (let i = startIndex; i < endIndex && i < cards.length; i++) {
+                    cards[i].style.display = 'block';
+                }
+            };
+
+            const updateActiveDot = (activeIndex) => {
+                // Remove active class from all dots
+                dots.forEach(dot => dot.classList.remove('active'));
+                // Add active class to the currently active dot
+                dots[activeIndex].classList.add('active');
+            };
+
+            // Initialize the first dot as active
+            updateActiveDot(0);
+        }
     });
+
+    explorerBody.appendChild(generateDiscoverMoreElement());
 }
 
+function generateHtmlForIdeasStrike() {
+    const htmlString = `<div class="ideas-strike">
+        <img src="https://www.icicidirect.com/Content/images/target.png" alt="target" loading="lazy" width="24" height="20">
+            <span> <p>Gladiator Stocks delivered&nbsp;<strong>79% success rate</strong></p> </span>
+            <img src="https://www.icicidirect.com/Content/images/target.png" alt="target" loading="lazy" width="24" height="20">
 
-function generateHtmlFromArray(companies) {
+    </div>`;
+    const div = document.createElement('div');
+    div.innerHTML = htmlString;
+    return div.firstElementChild;
+}
+
+function generateHtmlForCompanyCard(companies) {
     return companies.map(company => {
-        return `<div class="slide slick-slide slick-current slick-active" data-slick-index="1" aria-hidden="false" style="width: 302px; height: 279px;" tabindex="0" role="tabpanel" id="slick-slide11" aria-describedby="slick-slide-control11">
+        return `<div class="slide slick-slide slick-current slick-active" data-slick-index="1" aria-hidden="false" style="width: 280px; height: 279px;" tabindex="0" role="tabpanel" id="slick-slide11" aria-describedby="slick-slide-control11">
       <div class="box box-theme">
           <div class="heading_wrap">
               <h4 title="${company.name}">
@@ -112,29 +181,36 @@ function generateHtmlFromArray(companies) {
     })
 }
 
-// Example usage:
-const companies = [
-    {
-        name: "STEEL AUTHORITY OF INDIA LTD",
-        recoPrice: "128",
-        cmp: "130",
-        targetPrice: "127",
-        stopLoss: "129"
-    },
-    {
-        name: "L N T",
-        recoPrice: "188",
-        cmp: "190",
-        targetPrice: "127",
-        stopLoss: "129"
-    },
-    {
-        name: "STEEL AUTHORITY OF INDIA LTD",
-        recoPrice: "128",
-        cmp: "130",
-        targetPrice: "127",
-        stopLoss: "129"
-    },
 
-    // Add more objects for each company as needed
-];
+function generateDiscoverMoreElement() {
+    const htmlString = `<div class="mt-3 text-md-right text-center discover-more">
+                            <a href="https://www.icicidirect.com/research/equity/investing-ideas" class="link-color" target="_blank">Discover More <i class="icon-up-arrow icon"></i></a>
+                        </div>`;
+    const div = document.createElement('div');
+    div.innerHTML = htmlString;
+    return div.firstElementChild;
+
+}
+
+async function fetchAndProcessCompaniesData() {
+    const apiUrl = `${window.location.origin}/draft/anagarwa/tradingtips.json`;
+    try {
+        const response = await fetch(apiUrl);
+        console.log("response" + response);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        // Transform the API response to the desired companies array format
+        const companies = data.data.map(company => ({
+            name: company.CompanyName,
+            recoPrice: company.RecoPrice,
+            cmp: company.cmp,
+            targetPrice: company.target,
+            stopLoss: company.stoploss
+        }));
+        return companies;
+    } catch (error) {
+        console.error("Could not fetch companies data:", error);
+    }
+}
