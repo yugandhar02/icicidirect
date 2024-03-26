f99660c4947005c362f9bca182012b2ed4a2f271
Satya Deep Maheshwari <satyam@adobe.com>
2024-03-26 23:03:27 +0530

    feat: captcha

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,4 +4,20 @@ import { loadScript, sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+// This function is called when the Turnstile script is loaded and ready to be used.
+// The function name matches the "onload=..." parameter.
+// eslint-disable-next-line no-unused-vars
+function turnstileCb() {
+  document.querySelectorAll('.turnstile-container').forEach((el) => {
+    // eslint-disable-next-line no-undef
+    turnstile.render(el, {
+      sitekey: '0x4AAAAAAAVrq_k7QabpK5gM',
+      theme: 'light',
+    });
+  });
+}
+
+window.turnstileCb = turnstileCb;
+await loadScript('https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileCb');
+
 loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
