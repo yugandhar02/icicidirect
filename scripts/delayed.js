0bdf199d46d74e565c9430120bf0d9f92bb8b7b9
anagarwa <anagarwa@adobe.com>
2024-03-28 10:36:09 +0530

    fix: adding adobe dtm scripts

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,22 @@ import { loadScript, sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+function getEnvType(hostname = window.location.hostname) {
+  const fqdnToEnvType = {
+    'main--icicidirect--aemsites.hlx.page': 'preview',
+    'main--icicidirect--aemsites.hlx.live': 'live',
+  };
+  return fqdnToEnvType[hostname] || 'dev';
+}
+
 loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
 loadScript('/scripts/cookie-script.js');
 
-loadScript('https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83071cc-development.min.js', { async: true });
+(function loadAdobeLaunch() {
+  const adobeLaunchSrc = {
+    dev: 'https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83071cc-development.min.js',
+    preview: 'https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83071cc-development.min.js',
+    live: 'https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83071cc-development.min.js',
+  };
+  loadScript(adobeLaunchSrc[getEnvType()], { async: true });
+}());
