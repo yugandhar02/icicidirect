0e2f09890b6547c2516c744864a4a2e593b45767
anagarwa <anagarwa@adobe.com>
2024-03-28 09:32:35 +0530

    fix: adding gtm

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,8 +5,6 @@ import { loadScript, sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
-loadScript('/scripts/cookie-script.js');
-loadScript('https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83071cc-development.min.js', { async: true });
 
 (function gtm(w, d, s, l, i) {
   w[l] = w[l] || [];
@@ -22,7 +20,7 @@ loadScript('https://assets.adobedtm.com/64c36731dbac/390f7bab5b74/launch-285ee83
   f.parentNode.insertBefore(j, f);
 }(window, document, 'script', 'dataLayer', 'GTM-WF9LTLZ'));
 
-(function loadTagManagerElement() {
+(function loadTagManagerNoScript() {
   const htmlContent = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WF9LTLZ"\n'
       + 'height="0" width="0" style="display:none;visibility:hidden"></iframe>';
   const scriptElement = document.createElement('noscript');
