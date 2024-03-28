a21e5b1eb5e5299414202dd19e844e464ffc3553
Satya Deep Maheshwari <satyam@adobe.com>
2024-03-28 16:17:19 +0530

    test change

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -29,18 +29,29 @@ if (getEnvType() !== 'dev') {
 
 loadScript('https://icici-securities.allincall.in/files/deploy/embed_chatbot_11.js?version=1.1');
 
-setTimeout((function gtm(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js',
-  });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s);
-  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-}(window, document, 'script', 'dataLayer', 'GTM-WF9LTLZ')), 3000);
 
+/**
+ * Google Tag Manager
+* */
+async function loadGTM() {
+  const scriptTag = document.createElement('script');
+  scriptTag.innerHTML = `
+        (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+        }(window, document, 'script', 'dataLayer', 'GTM-WF9LTLZ'));
+    `;
+  document.head.prepend(scriptTag);
+}
+
+await loadGTM();
 loadScript('/scripts/cookie-script.js');
